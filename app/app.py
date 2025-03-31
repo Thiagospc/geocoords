aa47024157cfb150685a0a4aa45ba26b40d00e7d
Thiagospc <thiago.spc1029@gmail.com>
2025-03-31 18:46:30 -0300

    primeiro upload

---
+++ app/app.py
@@ -0,0 +1,111 @@
+# openpyxl
+import os
+import PySimpleGUIQt as sg
+import pandas as pd
+import folium
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+CAMINHO_ARQUIVO = os.path.join(BASE_DIR, "..", "arquivos", "dados.xlsx")
+
+def vizualizar_dados(caminho):
+    try:
+        if caminho.endswith('.csv'):
+            leitura = pd.read_csv(caminho, encoding='ISO-8859-1')  # Para CSV
+        elif caminho.endswith('.xlsx'):
+            leitura = pd.read_excel(caminho)  # Para Excel
+        else:
+            sg.popup_error("Formato de arquivo não suportado. Por favor, selecione um arquivo CSV ou Excel.")
+            return pd.DataFrame()
+        return leitura
+    except Exception as e:
+        sg.popup_error(f"Erro ao vizualizar dados: {e}")
+        return pd.DataFrame()
+    
+def adicionar_no_excel(id, nome, latitude, longitude, descricao):
+    try:
+        leitura = pd.read_excel(CAMINHO_ARQUIVO)
+
+        nova_linha = {
+            "ID": id,
+            "Nome": nome,
+            "Latitude": latitude,
+            "Longitude": longitude,
+            "Descrição": descricao
+        }
+        leitura = leitura.append(nova_linha, ignore_index=True)
+        leitura.to_excel(CAMINHO_ARQUIVO, index=False)
+
+        sg.popup("Dados adicionados com sucesso!")
+    except Exception as e:
+        sg.popup_error(f"Erro ao adicionar dados: {e}")
+
+def exibe_mapa(df):
+    pass
+
+def run_app():
+    sg.set_options(background_color="#90EE90")
+    estilo_botao = {"size": (12, 1), "border_width": 2}
+    headings = ["ID", "Nome", "Latitude", "Longitude", "Descrição"]
+    
+    layout = [
+        [sg.Text("Selecione um Arquivo CSV ou Excel:", font=("Arial", 10, "bold"))],
+        [sg.Input(size=(30, 1), key="-FILE-"), sg.FileBrowse("Importar", size=(10, 1))],
+        [
+            sg.Button("Vizualizar", **estilo_botao),
+            sg.Button("Editar", **estilo_botao),
+            sg.Button("Gerar Mapa", size=(16, 1), border_width=2),
+        ],
+        [sg.Table(
+                values=[[]], 
+                headings=headings,
+                auto_size_columns=False, 
+                justification="center", 
+                key="-TABLE-",
+                enable_events=True, col_widths=[10, 20, 15, 15, 30], num_rows=10,
+                alternating_row_color="lightblue"
+            )],
+        [sg.Text("Editar Linha (id, nome, latitude, longitude, descricao):")],
+        [sg.Input(size=(5, 1), key="-ID-"), sg.Input(size=(15, 1), key="-NOME-"),
+        sg.Input(size=(10, 1), key="-LATITUDE-"), sg.Input(size=(10, 1), key="-LONGITUDE-"),
+        sg.Input(size=(25, 1), key="-DESCRICAO-")],
+        [sg.Button("Adicionar", **estilo_botao)],
+        [sg.Button("Salvar Edição", **estilo_botao)]
+    ]
+
+    janela = sg.Window("GeoCoords", layout=layout)
+    
+    while True:
+        event, values = janela.read()
+        print(event, values)
+        
+        if event == sg.WIN_CLOSED:
+            break
+        if event == "Vizualizar":
+            caminho =  values["-FILE-"]
+            if caminho:
+                leitura = vizualizar_dados(caminho)
+                if not leitura.empty:
+                    valores_tabela = [["id", "nome", "latitude", "longitude", "descricao"]] + leitura.astype(str).values.tolist()
+                    janela["-TABLE-"].update(values=valores_tabela)
+                else:
+                    sg.popup_error("O arquivo está vazio ou não contém dados válidos.")
+
+        if event == "Adicionar":
+            id = values["-ID-"]
+            nome = values["-NOME-"]
+            latitude = values["-LATITUDE-"]
+            longitude = values["-LONGITUDE-"]
+            descricao = values["-DESCRICAO-"]
+
+            if id and nome and latitude and longitude and descricao:
+                adicionar_no_excel(id, nome, latitude, longitude, descricao)
+            else:
+                sg.popup_error("Preencha todos os campos antes de adicionar.")
+                    
+        if event == "Gerar Mapa":
+            pass
+
+    janela.close()
+
+if __name__ == "__main__":
+    run_app()
